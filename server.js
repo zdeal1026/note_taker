0389a7ba2cd1d546f0c1ecb1554568f7c3459bbf
Zdeal <jedimastershake@gmail.com>
2021-07-14 19:29:16 -0400

    continuing on routing

--- server.js
+++ server.js
@@ -2,10 +2,15 @@
 const fs = require('fs');
 const express = require('express');
 const path = require('path');
+const db = require('./db/db.json')
 
 //setting port
 const app = express();
 const PORT = 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //creating routes
 app.get('/', (req,res)=> {
@@ -13,5 +18,10 @@ app.get('/', (req,res)=> {
 });
 
 app.get('/notes', (req, res)=> {
-    res.sendfile(path.join(__dirname, 'public/notes.html'))
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
 });
+
+app.get('/api/notes', (req, res)=> {
+    const data = JSON.parse(fs.readFileSync('db/db.json', 'utf-8'));
+    res.json(data);
+})
