d6aec61bb4a9aeaaa655abab4edbdb68f909c737
Zdeal <jedimastershake@gmail.com>
2021-07-14 18:45:44 -0400

    starting index file

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,10 +1,10 @@
-let noteTitle;
-let noteText;
-let saveNoteBtn;
-let newNoteBtn;
-let noteList;
+let $noteTitle = $('.note-title');
+let $noteText = $('note.textarea');
+let $saveNoteBtn = $('.save-note');
+let $newNoteBtn= $('new-note');
+let $noteList = $('.list-container .list-group');
 
-if (window.location.pathname === '/notes') {
+if (window.location.pathname === './assets/notes') {
   noteTitle = document.querySelector('.note-title');
   noteText = document.querySelector('.note-textarea');
   saveNoteBtn = document.querySelector('.save-note');
