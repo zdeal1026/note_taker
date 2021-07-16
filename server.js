e9273c880e19f4913b5ef4575d305acd6cb65c3b
Zdeal <jedimastershake@gmail.com>
2021-07-15 21:34:57 -0400

    trouble shooting

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ app.get('/', (req,res)=> {
     res.sendFile(path.join(__dirname, 'public/assets/index.html'));
 });
 
-app.get('/notes', (req, res)=> {
-    res.sendFile(path.join(__dirname, 'public/notes.html'))
+app.get('/notes.html', (req, res)=> {
+    res.sendFile(path.join(__dirname, 'public/assets/notes.html'))
 });
 
 app.get('/api/notes', (req, res)=> {
