7066376b122932ba9e3bfb64050e3d24847bfc79
Zdeal <jedimastershake@gmail.com>
2021-08-16 21:46:41 -0400

    changing port

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const { v4: uuidv4 } = require('uuid')
 
 //setting port
 const app = express();
-const PORT = 3030;
+const PORT = 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
